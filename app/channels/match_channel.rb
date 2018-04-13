c8a98b527ac51d9d5cb90b572145c6080c3c4667
Andrew Arpasi <andrew@andrewarpasi.com>
2018-04-12 22:06:32 -0400

    Start match after timer

--- app/channels/match_channel.rb
+++ app/channels/match_channel.rb
@@ -4,18 +4,27 @@ class MatchChannel < ApplicationCable::Channel
 
   def subscribed
     #if not in matches, add to it
-    if !@@matches.key?(params[:match_id]) then
-      @@matches[params[:match_id]] = Hash.new
+    Thread.new do
+      if !@@matches.key?(params[:match_id]) then
+        @@matches[params[:match_id]] = Hash.new
+        puts "30 Seconds Until Match Start"
+        sleep 5 #change later
+        @@matches[params[:match_id]]["active"] = true
+        puts "Current Match #{@@matches[params[:match_id]]["active"]}"
+      end
     end
 
-    puts "Subscription for match #{params[:match_id]}" 
+    puts "Subscription for match #{params[:match_id]}"
+
+
+
 
     stream_from "match:#{params[:match_id]}"
 
   end
 
   def receive(data)
-    puts "Received WPM: #{data["wpm"]} id #{connection_identifier}"
+    puts "Received WPM: #{data["wpm"]}"
   end
 
   def unsubscribed
