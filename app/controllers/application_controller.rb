d200c26e72e210d1e4432364fb7fa09b2a2481cf
Andrew Arpasi <andrew@andrewarpasi.com>
2018-04-13 16:09:07 -0400

    Add display name for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :devise_sanitize_params, if: :devise_controller?
+
+    def devise_sanitize_params
+        registration_params = [:display_name, :email, :password, :password_confirmation]
+
+        if params[:action] == 'update' then
+            devise_parameter_sanitizer.permit(:account_update, keys: registration_params << :current_password)
+        elsif params[:action] == 'create' then
+            devise_parameter_sanitizer.permit(:sign_up, keys: registration_params)
+        end
+    end
 end
