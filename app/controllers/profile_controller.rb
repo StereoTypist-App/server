62623859a05a662a2d9005b52d725a493d0ae96b
Andrew Arpasi <andrew@andrewarpasi.com>
2018-04-14 16:58:26 -0400

    Validate user

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,11 +2,11 @@ class ProfileController < ApplicationController
     def profile
         if !params[:display_name] && current_user != nil then
             @user = current_user
-            @results = @user.results.order(wpm: :desc)
+            @results = @user.results.order(wpm: :desc) if @user
         elsif params[:display_name] != nil then
             @user = User.find_by(display_name: params[:display_name])
-            @results = @user.results.order(wpm: :desc)
+            @results = @user.results.order(wpm: :desc) if @user
         end
-        
+
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :results
 
+  validates :display_name, presence: true, uniqueness: true, format: { without: /\s/ }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
