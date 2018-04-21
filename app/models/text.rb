9bc5f59f8c79aa7528a6ba8c851e78a3660f719e
Andrew Arpasi <andrew@andrewarpasi.com>
2018-04-21 16:56:28 -0400

    Add validators to Text

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,3 +1,7 @@
 class Text < ApplicationRecord
+    validates :text, uniqueness: true, presence: true, format: { with: %r{\A[ A-Za-z0-9,\/\.;\?"'\-&\$!\(\))]+\z} }, length: {minimum: 600}
+    validates :user, presence: true
+    validates :title, presence: true, length: {minimum: 6, maximum: 40}
+
     belongs_to :user
 end
