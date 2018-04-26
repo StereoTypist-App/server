d9e0c1f2c421daf133d39a08f1a4ae7d5f672642
Andrew Arpasi <andrew@andrewarpasi.com>
2018-04-26 17:18:33 -0400

    Try newline to carriage return

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,5 +1,5 @@
 class Text < ApplicationRecord
-    validates :text, presence: true, format: { with: %r{\A[ A-Za-z0-9,\/\.:;\?"'\-&\$!\(\)\n]+\z} }, length: {minimum: 600}
+    validates :text, presence: true, format: { with: %r{\A[ A-Za-z0-9,\/\.:;\?"'\-&\$!\(\)\n\r]+\z} }, length: {minimum: 600}
     validates :user, presence: true
     validates :title, presence: true, length: {minimum: 6, maximum: 40}
 
@@ -7,6 +7,6 @@ class Text < ApplicationRecord
 
     def self.get_text_array()
         text = Text.order("RANDOM()").limit(1)[0].text
-        return text.split('\n',-1)
+        return text.split('\r',-1)
     end
 end
